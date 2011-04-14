0e998c5578d108488407871fd922c2cc3ce7c1d9
Jeremy Green <jagthedrummer@gmail.com>
2011-04-14 16:03:50 -0500

    Upgraded to version 0.0.11 of codemirror-ui

--- js/handmirror.js
+++ js/handmirror.js
@@ -24,22 +24,22 @@ function onLoadInit() {
 	var textarea = document.getElementById('htmlSource');
 	  //CodeMirror.replace(textarea);
 	  
-	mirror = new CodeMirrorUI(textarea,
+	var ui = new CodeMirrorUI(textarea,
 	   {
 	  	path : 'js/codemirror-ui/js/'
 	   },
 	   {
-	    height: "150px",
-	    content: textarea.value,
-	    parserfile: ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"],
-	    stylesheet: ["js/codemirror-ui/lib/CodeMirror-0.93/css/xmlcolors.css", "js/codemirror-ui/lib/CodeMirror-0.93/css/jscolors.css", "js/codemirror-ui/lib/CodeMirror-0.93/css/csscolors.css"],
-	    path: "js/codemirror-ui/lib/CodeMirror-0.93/js/",
-		reindentOnLoad : true,
-	    autoMatchParens: true,
-		disableSpellcheck : true,
-		lineNumbers : true
+	    matchBrackets: true,
+      lineNumbers : true,
+      mode : "text/html",
+      onCursorActivity: function() {
+       editor.setLineClass(hlLine, null);
+       hlLine = editor.setLineClass(editor.getCursor().line, "activeline");
+      }
+      
 	  });
-
+  var editor = ui.mirror;
+  hlLine = editor.setLineClass(0, "activeline");
 	resizeInputs();
 }
 
@@ -49,7 +49,7 @@ function findCodeMirror(){
 	elems = document.getElementById('areaWrap').childNodes;
 	for(var i=0; i<elems.length;i++){
 		el = elems[i]
-		if(el.className=='CodeMirror-wrapping'){
+		if(el.className=='CodeMirror'){
 			return el
 		}
 	}
